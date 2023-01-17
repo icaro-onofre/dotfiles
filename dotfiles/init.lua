2027cbcbeda490a719e5b1ff6a29f205c6eca46c
Icaro <icaro.onofre.s@gmail>
2023-01-17 16:06:40 -0300

    updating dotfiles

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -4,7 +4,10 @@ require "user.plugins"
 require "user.colorscheme"
 require "user.cmp"
 require "user.lsp"
-require("configs.settings")
+require "configs.settings"
+
+vim.o.termiguicolors = true
+vim.o.errorbells = false
 vim.o.background='light'
 vim.wo.colorcolumn='80'
 vim.g.mapleader='TAB'
