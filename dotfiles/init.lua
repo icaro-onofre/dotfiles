23e143881a90ac8211ad8f6096bc7197d5a7bf5c
Icaro <icaro.onofre.s@gmail>
2023-01-17 15:05:03 -0300

    updating dotfiles

---
+++ dotfiles/init.lua
@@ -0,0 +1,10 @@
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
+require "user.colorscheme"
+require "user.cmp"
+require "user.lsp"
+require("configs.settings")
+vim.o.background='light'
+vim.wo.colorcolumn='80'
+vim.g.mapleader='TAB'
