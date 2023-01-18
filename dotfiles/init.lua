e106906a18ad69186b61e67852146c098a5e6111
Icaro <icaro.onofre.s@gmail>
2023-01-18 13:41:08 -0300

    Updating lua plugin

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -1,13 +1,35 @@
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.colorscheme"
-require "user.cmp"
-require "user.lsp"
-require "configs.settings"
 
-vim.o.termiguicolors = true
-vim.o.errorbells = false
-vim.o.background = 'light'
-vim.wo.colorcolumn = '80'
-vim.g.mapleader = 'TAB'
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use 'nvim-telescope/telescope.nvim'
+  -- My plugins here
+  -- use 'foo1/bar1.nvim'
+  -- use 'foo2/bar2.nvim'
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
+
+
+--Ui configurations
+--vim.o.termiguicolors = true
+--vim.o.errorbells = false
+--vim.o.background = 'light'
+--vim.wo.colorcolumn = '80'
+--vim.g.mapleader = 'TAB'
