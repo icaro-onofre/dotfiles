be3dd3a042505a2226515873fa73c1ed2c6638e6
fforelle <xdollboyx@gmail.com>
2023-03-15 07:23:35 -0300

    Updating dotfiles

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -10,8 +10,6 @@ vim.g.mapleader=' '
 vim.opt.number = true
 vim.opt.swapfile = false
 
-vim.bo.tabstop = 4
-
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
