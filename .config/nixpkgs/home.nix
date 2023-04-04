c5919d3fd3b85675662dbb4edbdd94b1d5688028
Icaro <icaro.onofre.s@gmail.com>
2023-04-04 09:27:29 -0300

    Adding missing .config files

---
+++ .config/nixpkgs/home.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "fforelle";
+  home.homeDirectory = "/home/fforelle";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  home.packages =  [ 
+		pkgs.fortune
+  ];
+
+}
