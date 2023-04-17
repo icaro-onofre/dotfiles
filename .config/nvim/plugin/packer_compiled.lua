f31444bea43a8efce6849590110c6ec773a0acc4
Icaro <icaro.onofre.s@gmail.com>
2023-04-17 09:38:35 -0400

    Resolving stash conflict

--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -79,108 +79,10 @@ _G.packer_plugins = {
     path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/DAPInstall.nvim",
     url = "https://github.com/Pocco81/DAPInstall.nvim"
   },
-  LuaSnip = {
-    config = { "\27LJ\2\nO\0\0\2\1\2\0\t-\0\0\0009\0\0\0B\0\1\2\15\0\0\0X\1\3€-\0\0\0009\0\1\0B\0\1\1K\0\1\0\0À\19expand_or_jump\23expand_or_jumpableë\2\1\0\t\0\19\1\0306\0\0\0'\2\1\0B\0\2\0029\1\2\0'\3\3\0004\4\3\0009\5\4\0009\5\5\5'\a\6\0'\b\a\0B\5\3\0?\5\0\0B\1\3\0016\1\b\0009\1\t\0019\1\n\1'\3\v\0'\4\f\0003\5\r\0B\1\4\0016\1\0\0'\3\14\0B\1\2\0029\1\15\0015\3\17\0005\4\16\0=\4\18\3B\1\2\0012\0\0€K\0\1\0\npaths\1\0\0\1\2\0\0J~/.local/share/nvim/site/pack/packer/start/friendly-snippets/snippets\tload luasnip/loaders/from_vscode\0\n<c-p>\6i\bset\vkeymap\bvim#function ${1}(${2}) \n{\n\t${3}\n}\tfunc\18parse_snippet\vparser\ball\17add_snippets\fluasnip\frequire\3€€À™\4\0" },
+  neotest = {
     loaded = true,
-    path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/LuaSnip",
-    url = "https://github.com/L3MON4D3/LuaSnip"
-  },
-  ["bluloco.nvim"] = {
-    loaded = true,
-    path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/bluloco.nvim",
-    url = "https://github.com/uloco/bluloco.nvim"
-  },
-  catppuccin = {
-    loaded = true,
-    path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/catppuccin",
-    url = "https://github.com/catppuccin/nvim"
-  },
-  ["cmp-nvim-lsp"] = {
-    loaded = true,
-    path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
-    url = "https://github.com/hrsh7th/cmp-nvim-lsp"
-  },
-  ["color-picker.nvim"] = {
-    loaded = true,
-    path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/color-picker.nvim",
-    url = "https://github.com/ziontee113/color-picker.nvim"
-  },
-  ["diffview.nvim"] = {
-    loaded = true,
-    path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/diffview.nvim",
-    url = "https://github.com/sindrets/diffview.nvim"
-  },
-  ["document-color.nvim"] = {
-    config = { "\27LJ\2\nS\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\1\tmode\15background\nsetup\19document-color\frequire\0" },
-    loaded = true,
-    path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/document-color.nvim",
-    url = "https://github.com/mrshmllow/document-color.nvim"
-  },
-  ["emmet-vim"] = {
-    loaded = true,
-    path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/emmet-vim",
-    url = "https://github.com/mattn/emmet-vim"
-  },
-  ["friendly-snippets"] = {
-    loaded = true,
-    path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/friendly-snippets",
-    url = "https://github.com/rafamadriz/friendly-snippets"
-  },
-  ["gitsigns.nvim"] = {
-    config = { "\27LJ\2\n6\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\rgitsigns\frequire\0" },
-    loaded = true,
-    path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
-    url = "https://github.com/lewis6991/gitsigns.nvim"
-  },
-  ["lsp-zero.nvim"] = {
-    loaded = true,
-    path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
-    url = "https://github.com/VonHeikemen/lsp-zero.nvim"
-  },
-  ["lush.nvim"] = {
-    loaded = true,
-    path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/lush.nvim",
-    url = "https://github.com/rktjmp/lush.nvim"
-  },
-  ["marks.nvim"] = {
-    loaded = true,
-    path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/marks.nvim",
-    url = "https://github.com/chentoast/marks.nvim"
-  },
-  ["mason-lspconfig.nvim"] = {
-    loaded = true,
-    path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
-    url = "https://github.com/williamboman/mason-lspconfig.nvim"
-  },
-  ["mason.nvim"] = {
-    loaded = true,
-    path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/mason.nvim",
-    url = "https://github.com/williamboman/mason.nvim"
-  },
-  ["nerdtree-visual-selection"] = {
-    loaded = true,
-    path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/nerdtree-visual-selection",
-    url = "https://github.com/PhilRunninger/nerdtree-visual-selection"
-  },
-  ["noctis.nvim"] = {
-    loaded = true,
-    path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/noctis.nvim",
-    url = "https://github.com/kartikp10/noctis.nvim"
-  },
-  ["null-ls.nvim"] = {
-    loaded = true,
-    path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/null-ls.nvim",
-    url = "https://github.com/jose-elias-alvarez/null-ls.nvim"
-  },
-  ["nvim-cmp"] = {
-    loaded = true,
-    path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/nvim-cmp",
-    url = "https://github.com/hrsh7th/nvim-cmp"
-  },
-  ["nvim-colorizer.lua"] = {
-    loaded = true,
-    path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/nvim-colorizer.lua",
-    url = "https://github.com/norcalli/nvim-colorizer.lua"
+    path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/neotest",
+    url = "https://github.com/nvim-neotest/neotest"
   },
   ["nvim-dap"] = {
     loaded = true,
@@ -197,35 +99,10 @@ _G.packer_plugins = {
     path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/nvim-dap-virtual-text",
     url = "https://github.com/theHamsta/nvim-dap-virtual-text"
   },
-  ["nvim-ipy"] = {
-    loaded = true,
-    path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/nvim-ipy",
-    url = "https://github.com/bfredl/nvim-ipy"
-  },
-  ["nvim-lspconfig"] = {
-    loaded = true,
-    path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
-    url = "https://github.com/neovim/nvim-lspconfig"
-  },
-  ["nvim-tree.lua"] = {
-    loaded = true,
-    path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
-    url = "https://github.com/nvim-tree/nvim-tree.lua"
-  },
-  ["nvim-treesitter"] = {
-    loaded = true,
-    path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
-    url = "https://github.com/nvim-treesitter/nvim-treesitter"
-  },
   ["nvim-ts-rainbow"] = {
     loaded = true,
     path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/nvim-ts-rainbow",
     url = "https://github.com/p00f/nvim-ts-rainbow"
-  },
-  ["nvim-web-devicons"] = {
-    loaded = true,
-    path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
-    url = "https://github.com/nvim-tree/nvim-web-devicons"
   },
   ["packer.nvim"] = {
     loaded = true,
@@ -237,11 +114,6 @@ _G.packer_plugins = {
     path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
-  ["prettier.nvim"] = {
-    loaded = true,
-    path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/prettier.nvim",
-    url = "https://github.com/MunifTanjim/prettier.nvim"
-  },
   ["telescope-dap.nvim"] = {
     loaded = true,
     path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/telescope-dap.nvim",
@@ -252,46 +124,14 @@ _G.packer_plugins = {
     path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
-  ["tokyonight.nvim"] = {
-    loaded = true,
-    path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",
-    url = "https://github.com/folke/tokyonight.nvim"
-  },
-  ["vim-devicons"] = {
-    loaded = true,
-    path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/vim-devicons",
-    url = "https://github.com/ryanoasis/vim-devicons"
-  },
-  ["vim-fugitive"] = {
-    loaded = true,
-    path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/vim-fugitive",
-    url = "https://github.com/tpope/vim-fugitive"
-  },
   ["vim-snippets"] = {
     loaded = true,
     path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/vim-snippets",
     url = "https://github.com/honza/vim-snippets"
-  },
-  ["vim-surround"] = {
-    loaded = true,
-    path = "/home/fforelle/.local/share/nvim/site/pack/packer/start/vim-surround",
-    url = "https://github.com/tpope/vim-surround"
   }
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: LuaSnip
-time([[Config for LuaSnip]], true)
-try_loadstring("\27LJ\2\nO\0\0\2\1\2\0\t-\0\0\0009\0\0\0B\0\1\2\15\0\0\0X\1\3€-\0\0\0009\0\1\0B\0\1\1K\0\1\0\0À\19expand_or_jump\23expand_or_jumpableë\2\1\0\t\0\19\1\0306\0\0\0'\2\1\0B\0\2\0029\1\2\0'\3\3\0004\4\3\0009\5\4\0009\5\5\5'\a\6\0'\b\a\0B\5\3\0?\5\0\0B\1\3\0016\1\b\0009\1\t\0019\1\n\1'\3\v\0'\4\f\0003\5\r\0B\1\4\0016\1\0\0'\3\14\0B\1\2\0029\1\15\0015\3\17\0005\4\16\0=\4\18\3B\1\2\0012\0\0€K\0\1\0\npaths\1\0\0\1\2\0\0J~/.local/share/nvim/site/pack/packer/start/friendly-snippets/snippets\tload luasnip/loaders/from_vscode\0\n<c-p>\6i\bset\vkeymap\bvim#function ${1}(${2}) \n{\n\t${3}\n}\tfunc\18parse_snippet\vparser\ball\17add_snippets\fluasnip\frequire\3€€À™\4\0", "config", "LuaSnip")
-time([[Config for LuaSnip]], false)
--- Config for: document-color.nvim
-time([[Config for document-color.nvim]], true)
-try_loadstring("\27LJ\2\nS\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\1\tmode\15background\nsetup\19document-color\frequire\0", "config", "document-color.nvim")
-time([[Config for document-color.nvim]], false)
--- Config for: gitsigns.nvim
-time([[Config for gitsigns.nvim]], true)
-try_loadstring("\27LJ\2\n6\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\rgitsigns\frequire\0", "config", "gitsigns.nvim")
-time([[Config for gitsigns.nvim]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
