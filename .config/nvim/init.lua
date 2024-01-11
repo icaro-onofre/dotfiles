bd96da65fa96b1884e1a10348e6ec74ba9a20f7e
Icaro <icaro.onofre.s@gmail.com>
2024-01-11 20:54:53 -0300

    updating dotfiles

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -18,5 +18,26 @@ require("lazy").setup({
 "sindrets/diffview.nvim",
 "folke/tokyonight.nvim",
 "nvim-treesitter/nvim-treesitter",
+{
+	'nvim-telescope/telescope.nvim', tag = '0.1.5',
+	dependencies = { 'nvim-lua/plenary.nvim' },
+},
+
 })
 
+-- Set leader key
+vim.g.mapleader = "<Space>"
+-- Basic UI configurations
+vim.opt.termguicolors = true  	
+vim.opt.shiftwidth=4
+vim.opt.tabstop=4
+vim.cmd 'colorscheme tokyonight-storm' -- select this colorscheme if it is installed
+vim.cmd 'set rnu'
+-- Telescope keymaps
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff',builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg',builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb',builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh',builtin.help_tags, {})
+-- Set no swap
+vim.opt.swapfile = false
