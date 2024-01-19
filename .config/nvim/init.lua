8db99d218670ce41eef6d3b583d67f6b5a2ec984
Icaro <icaro.onofre.s@gmail.com>
2024-01-19 16:15:11 -0300

    Diminuindo config do neovim,add quickmarks

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,13 +1,71 @@
---require "core.nvimsurround"
-require "core.mason"
-require "core.dbg"
-require "core.nvimtree"
-require "core.treesitter"
---require "core.cmp"
-require "core.lsp"
-require "core.marks"
-require "core.plugins"
-require "core.keymaps"
-require "core.options"
+-- Icaro's Neovim conifguration
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
 
-require 'colorizer'.setup()
+require("lazy").setup({
+	"folke/which-key.nvim",
+	{ "folke/neoconf.nvim", cmd = "Neoconf" },
+	"folke/neodev.nvim",
+	"sindrets/diffview.nvim",
+	"folke/tokyonight.nvim",
+	"nvim-treesitter/nvim-treesitter",
+	"nvim-treesitter/nvim-treesitter",
+	"HiPhish/rainbow-delimiters.nvim",
+	"tpope/vim-fugitive",
+	{
+		'nvim-telescope/telescope.nvim', tag = '0.1.5',
+		dependencies = { 'nvim-lua/plenary.nvim' },
+	},
+	-- LSP Zero configuration
+	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+	{'neovim/nvim-lspconfig'},
+	{'hrsh7th/cmp-nvim-lsp'},
+	{'hrsh7th/nvim-cmp'},
+	{'L3MON4D3/LuaSnip'},
+
+})
+
+-- Set leader key
+vim.g.mapleader = " "
+-- Basic UI configurations
+vim.opt.termguicolors = true  	
+vim.opt.shiftwidth=4
+vim.opt.tabstop=4
+vim.cmd 'colorscheme tokyonight-storm' -- select this colorscheme if it is installed
+vim.cmd 'set rnu'
+vim.wo.relativenumber = true
+-- treesitter configurations 
+require'nvim-treesitter.configs'.setup {
+	-- A list of parser names, or "all" (the five listed parsers should always be installed)
+	ensure_installed = {"c", "lua", "vim", "vimdoc", "query" },
+	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+	auto_install = true,
+	-- List of parsers to ignore installing (or "all")
+	ignore_install = { "javascript" },
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = true,
+	},
+}
+-- Telescope keymaps
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff',builtin.find_files, {})
+vim.keymap.set('n', '<leader>fp',builtin.git_files, {})
+vim.keymap.set('n', '<leader>fg',builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb',builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh',builtin.help_tags, {})
+-- diff view keybindings
+--vim.keymap.set('n', '<leader>gd',DiffViewOpen, {})
+--vim.keymap.set('n', '<leader>gq',DiffViewClose, {})
+-- Set no swap
+vim.opt.swapfile = false
--- .config/nvim/lua/core/options.lua
+++
@@ -1,6 +0,0 @@
-vim.cmd('colorscheme catppuccin-mocha')
-vim.cmd('set rnu')
-
-vim.opt.termguicolors = true
-vim.opt.number = true
-vim.opt.swapfile = false
