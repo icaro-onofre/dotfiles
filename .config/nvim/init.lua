b0654f99b2387cb9cd8d418a60be589dbdeba495
Icaro <icaro.onofre.s@gmail.com>
2024-09-14 11:36:43 -0300

    updating dotfiles

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -168,6 +168,9 @@ vim.keymap.set('n', '<leader>fm',builtin.marks, {})
 -- Lsp telescope
 vim.keymap.set('n', '<leader>fr',builtin.lsp_references, {})
 
+-- vim.keymap.set('n', '<leader>fc',builtin.telescope.builtin.colorscheme, {})
+
+
 -- diff view keybindings
 vim.keymap.set('n', '<leader>gd',"<Cmd>DiffviewOpen<CR>", {})
 vim.keymap.set('n', '<leader>gq',"<Cmd>DiffviewClose<CR>", {})
@@ -210,7 +213,8 @@ require('lspconfig').tailwindcss.setup({})
 -- Run sudo npm install --save vscode-html-languageservice
 require('lspconfig').tsserver.setup({})
 -- java language server
-require'lspconfig'.java_language_server.setup{}
+require'lspconfig'.jdtls.setup{}
+-- install jdtls with yay -S jdtls
 -- HTML language server 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
@@ -279,7 +283,7 @@ require("dap-vscode-js").setup({
   -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
   -- debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug", -- Path to vscode-js-debug installation.
   -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-  adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
+  adapters = { 'chrome','pwa-chrome'}, -- which adapters to register in nvim-dap
   -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
   -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
   -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
@@ -290,7 +294,7 @@ local dap = require('dap')
 
 dap.configurations['typescriptreact'] = {
 	{
-		type = 'pwa-node',
+		type = 'pwa-chrome',
 		request = 'launch',
 		name = 'Launch file',
 		program = '${file}',
