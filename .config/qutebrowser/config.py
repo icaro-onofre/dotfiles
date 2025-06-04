4c7e2b1ed82ee8fa24ffa63275912618db54eba4
Icaro <icaro.onofre.s@gmail.com>
2025-06-04 17:01:58 -0300

    updating dotfiles

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,6 +1,4 @@
 config.load_autoconfig(False)
-c.tabs.position = "left"
-c.tabs.width = 5
-c.colors.tabs.bar.bg = "#000000"
-
-
+# c.tabs.position = "left"
+# c.tabs.width = 5
+# c.colors.tabs.bar.bg = "#000000"
